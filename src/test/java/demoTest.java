1f48db16eb8cd153bfb39651ec8096e421fe7a34
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-10-10 12:27:28 +0600

    New Changes

--- src/test/java/demoTest.java
+++ src/test/java/demoTest.java
@@ -8,14 +8,14 @@ public class demoTest {
         System.setProperty("webdriver.chrome.driver","E:\\RL QA\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://alignmedical.com/");
-        driver.findElement(By.xpath("//a[contains(text(),'HOME')]")).click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//a[contains(text(),'LOCATIONS')]")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//a[contains(text(),'CAREER')]")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("(//a[text()=\"Contact Us \"])[1]")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.quit();
+//        driver.findElement(By.xpath("//a[contains(text(),'HOME')]")).click();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        driver.findElement(By.xpath("//a[contains(text(),'LOCATIONS')]")).click();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.findElement(By.xpath("//a[contains(text(),'CAREER')]")).click();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.findElement(By.xpath("(//a[text()=\"Contact Us \"])[1]")).click();
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        driver.quit();
     }
 }
